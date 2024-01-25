b2fae0783db85b904005e48a7316c6c6d05d469e
carlosrobert0 <carlosroberto15755@gmail.com>
2024-01-25 12:25:56 -0300

    fx

--- src/controllers/chapter/upload-chapter.controller.ts
+++ src/controllers/chapter/upload-chapter.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, FileTypeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { Controller, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { Public } from "../../auth/public";
 import { ChapterService } from "../../chapter/chapter.service";
@@ -11,11 +11,11 @@ export class UploadChapterController {
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   async handle(@UploadedFile(
-    new ParseFilePipe({
-      validators: [
-        new FileTypeValidator({ fileType: '.(audio/wav|audio/mp3|image/jpeg|image/png|image/jpg)' }),
-      ],
-    }),
+    // new ParseFilePipe({
+    //   validators: [
+    //     new FileTypeValidator({ fileType: '.(audio/wav|audio/mp3|image/jpeg|image/png|image/jpg)' }),
+    //   ],
+    // }),
   ) file: Express.Multer.File) {
     const { originalname, mimetype, buffer } = file
 
